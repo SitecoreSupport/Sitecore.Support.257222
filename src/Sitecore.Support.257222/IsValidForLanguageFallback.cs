90f4b8ee79892034f57a8b3d3dbfe69ac369fd91
DK\ach <ach@sitecore.com>
2018-10-03 15:25:52 +0300

    FIx the issue with UiStatic field

--- src/Sitecore.Support.257222/IsValidForLanguageFallback.cs
+++ src/Sitecore.Support.257222/IsValidForLanguageFallback.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using Sitecore.Data.Fields;
 using Sitecore.Data.LanguageFallback;
 using Sitecore.Pipelines.GetFieldValue;
 
@@ -13,8 +14,18 @@ namespace Sitecore.Support.Pipelines.GetFieldValue.IsValidForLanguageFallback
     {
       if (args.AllowFallbackValue)
       {
-        args.IsValidForLanguageFallback = LanguageFallbackFieldValuesManager.IsValidForFallback(args.Field);
+        args.IsValidForLanguageFallback = !IsUiStatic(args.Field);
+
+        if (args.IsValidForLanguageFallback)
+        {
+          args.IsValidForLanguageFallback = LanguageFallbackFieldValuesManager.IsValidForFallback(args.Field);
+        }
       }
+    }
+
+    private bool IsUiStatic(Field field)
+    {
+      return field.ID == FieldIDs.UIStaticItem;
     }
   }
 }
