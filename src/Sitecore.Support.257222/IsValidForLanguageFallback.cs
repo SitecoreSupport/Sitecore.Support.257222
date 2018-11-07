b3a51f03aa7be1f33c3f9e99e935effb3f98c0d5
AriaAkbariyeh <aria.ak@hotmail.com>
2018-11-07 08:12:57 +0800

    Fix the issue with old layout field (closes #1)

---
+++ src/Sitecore.Support.257222/IsValidForLanguageFallback.cs
@@ -0,0 +1,35 @@
+﻿using Sitecore.Data;
+using Sitecore.Data.Fields;
+using Sitecore.Data.LanguageFallback;
+using Sitecore.Pipelines.GetFieldValue;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sitecore.Support.Pipelines.GetFieldValue
+{
+  public class IsValidForLanguageFallback
+  {
+    public void Process(GetFieldValueArgs args)
+    {
+      if (args.AllowFallbackValue)
+      {
+        if (!IsUiStatic(args.Field) && !IsOldLayout(args.Field))
+        {
+          args.IsValidForLanguageFallback = LanguageFallbackFieldValuesManager.IsValidForFallback(args.Field);
+        }
+      }
+    }
+
+    private bool IsUiStatic(Field field)
+    {
+      return field.ID == FieldIDs.UIStaticItem;
+    }
+
+    private bool IsOldLayout(Field field)
+    {
+      return field.ID == new ID("{E1D68787-D22B-4EA2-82B3-84C282E375EB}");
+    }
+  }
+}
